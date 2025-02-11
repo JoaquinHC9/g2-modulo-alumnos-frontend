cc967758cfaf911bb75e9831fb67e3fd3929183f
Joaquin Hidalgo <113700898+JoaquinHC9@users.noreply.github.com>
2025-02-11 10:01:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:/g2-modulo-alumnos-frontend/,
+  base:'/g2-modulo-alumnos-frontend/',
   resolve: {
     alias: {
       '@': '/src'
